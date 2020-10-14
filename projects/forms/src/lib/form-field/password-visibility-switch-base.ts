67f932d7fb581cbbaf1b4ee41f9722b147806a0d
lukasalphta <lukasz@alphta.de>
2020-10-14 14:00:43 +0200

    feat: init demo project and fix password visibility base

--- projects/forms/src/lib/form-field/password-visibility-switch-base.ts
+++ projects/forms/src/lib/form-field/password-visibility-switch-base.ts
@@ -3,13 +3,16 @@ import { EventEmitter, Directive, Output, HostListener } from '@angular/core';
 @Directive()
 // tslint:disable-next-line:directive-class-suffix
 export class PasswordVisibilitySwitchBase {
-  private shouldBePassVisible = false;
+  private _shouldBePassVisible = false;
+  get shouldBePassVisible(): boolean {
+    return this._shouldBePassVisible;
+  }
 
   @Output() visibilityChange: EventEmitter<boolean> = new EventEmitter();
 
   @HostListener('click', ['$event.target'])
-  onPasswordVisibilityChange() {
-    this.shouldBePassVisible = !this.shouldBePassVisible;
-    this.visibilityChange.emit(this.shouldBePassVisible);
+  onPasswordVisibilityChange(): void {
+    this._shouldBePassVisible = !this.shouldBePassVisible;
+    this.visibilityChange.emit(this._shouldBePassVisible);
   }
 }
